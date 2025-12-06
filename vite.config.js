7d614cb47fc45c09a7ca0f2f1638095c385e40fb
James Michael Hunter Turner <michael@turneratlanta.com>
2025-12-05 23:53:35 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
-          { allowedHosts: "zp2tqg-5173.csb.app" },
-  build: { outDir: 'dist' }
+  server: {
+    port: 5173,
+    allowedHosts: "zp2tqg-5173.csb.app"
+  },
+  build: {
+    outDir: 'dist'
+  }
 });
