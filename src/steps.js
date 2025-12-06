d3c26357e69be6c96eb1d350971e897cfd147ee5
James Michael Hunter Turner <michael@turneratlanta.com>
2025-12-05 21:33:26 -0500

    Create steps.js

---
+++ src/steps.js
@@ -0,0 +1,8 @@
+export const steps = [
+  { id: 0, name: 'Concept' },
+  { id: 1, name: 'Entity' },
+  { id: 2, name: 'Register' },
+  { id: 3, name: 'Marketing' },
+  { id: 4, name: 'Finance' },
+  { id: 5, name: 'Website' }
+];
