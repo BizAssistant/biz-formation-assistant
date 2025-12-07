71b695552bf8c2d7de3638614dbbf1c8793aebeb
James Michael Hunter Turner <michael@turneratlanta.com>
2025-12-07 15:43:47 -0500

    Update upload-report.jsj

--- functions/api/upload-report.js
+++ functions/api/upload-report.js
@@ -5,6 +5,12 @@ export async function onRequest(context) {
   const objectName = `report-${Date.now()}.pdf`;
 
   await env.REPORTS_BUCKET.put(objectName, body);
+
+  // Log action
+  const id = env.AUDIT_LOGGER.idFromName("global");
+  const obj = env.AUDIT_LOGGER.get(id);
+  await obj.fetch(`https://audit?action=upload&user=contributor`);
+
   return new Response(JSON.stringify({ key: objectName }), {
     headers: { "content-type": "application/json" },
   });
