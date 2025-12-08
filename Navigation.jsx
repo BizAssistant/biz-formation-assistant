f8fa541f359c92bcbd689accace12f82627bd119
James Michael Hunter Turner <michael@turneratlanta.com>
2025-12-08 04:22:49 -0500

    Create Navigation.jsx

---
+++ Navigation.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+export default function Navigation() {
+  const [domainOpen, setDomainOpen] = useState(false);
+
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/onboarding">Onboarding</a></li>
+        <li
+          onMouseEnter={() => setDomainOpen(true)}
+          onMouseLeave={() => setDomainOpen(false)}
+        >
+          <a href="#domains">Domains</a>
+          {domainOpen && (
+            <ul className="submenu">
+              <li><a href="/domains/search">Search Domain</a></li>
+              <li><a href="/domains/register">Register Domain</a></li>
+              <li><a href="/domains/manage">Manage Domains</a></li>
+            </ul>
+          )}
+        </li>
+        <li><a href="/settings">Settings</a></li>
+      </ul>
+    </nav>
+  );
+}
