ea8a50c31cc421ad06adab7f05c0a7ff12ba1135
James Michael Hunter Turner <michael@turneratlanta.com>
2025-12-09 14:51:52 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE = "bizform-v1";
-const CORE = ["/", "/index.html", "/manifest.webmanifest", "/public/logo.png"};
+const CACHE = 'bizform-v1';
+const CORE = ['/', '/index.html', '/manifest.webmanifest', '/assets/logo.png'];
 
-self.addEventListener("install", (event) => {
+self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(CORE)));
 });
-self.addEventListener("fetch", (event) => {y
+
+self.addEventListener('fetch', (event) => {
   event.respondWith(caches.match(event.request).then((res) => res || fetch(event.request)));
 });
