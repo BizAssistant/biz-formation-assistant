9acc4a8ab38a541a5c86a0f0c925d850f34aab33
James Michael Hunter Turner <michael@turneratlanta.com>
2025-12-09 15:54:41 -0500

    Update cleanup.js

--- functions/api/cleanup.js
+++ functions/api/cleanup.js
@@ -1,11 +1,11 @@
 // functions/api/cleanup.js
 export async function onRequest(context) {
   const { env } = context;
-  const list = await env.REPORTS_BUCKET.list();
+  const list = await env.BIZFORMS_R2.list();
 
   for (const obj of list.objects) {
     if (Date.now() - new Date(obj.uploaded).getTime() > 7 * 24 * 60 * 60 * 1000) {
-      await env.REPORTS_BUCKET.delete(obj.key);
+      await env.BIZFORMS_R2.delete(obj.key);
     }
   }
 
