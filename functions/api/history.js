e977a7f783871d972963451c38c07ca81ef4d352
James Michael Hunter Turner <michael@turneratlanta.com>
2025-12-09 15:56:46 -0500

    Update history.js

--- functions/api/history.js
+++ functions/api/history.js
@@ -1,7 +1,7 @@
 // functions/api/history.js
 export async function onRequest(context) {
   const { env } = context;
-  const list = await env.REPORTS_BUCKET.list();
+  const list = await env.BIZFORMS_R2.list();
   return new Response(JSON.stringify(list.objects), {
     headers: { "content-type": "application/json" },
   });
