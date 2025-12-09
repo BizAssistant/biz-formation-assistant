75bae619f1e68c277ec98865363e68fbb67363f6
James Michael Hunter Turner <michael@turneratlanta.com>
2025-12-09 14:55:21 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,19 +6,18 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        // Add any additional options here
+        // additional options
       },
     },
-  },
-  server: {
-    port: 3000,
-    open: true,
-    allowedHosts: "zp2tqg-5173.csb.app" // If you need this for CodeSandbox, keep it
   },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
+  },
+  server: {
+    port: 3000,
+    open: true,
   },
   preview: {
     port: 3000,
