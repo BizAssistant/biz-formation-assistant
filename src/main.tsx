54abe66669cea247fc22c2f390783212f24c950b
James Michael Hunter Turner <michael@turneratlanta.com>
2025-12-10 09:08:09 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import './styles/Main.scss';
+import './styles/main.scss';
 
 const container = document.getElementById('app');
 
