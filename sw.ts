80273d8086d85cb25a6b25969f123d96846e9679
James Michael Hunter Turner <michael@turneratlanta.com>
2025-12-09 20:38:06 -0500

    Update sw.ts

--- sw.ts
+++ sw.ts
@@ -4,6 +4,6 @@ const CORE = ["/", "/index.html", "/manifest.webmanifest", "/assets/logo.png"};
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(CORE)));
 });
-self.addEventListener("fetch", (event) => {y
+self.addEventListener("fetch", (event) => {
   event.respondWith(caches.match(event.request).then((res) => res || fetch(event.request)));
 });
