6ad8f93f5aab2623642b954192102d87eb0ee6f5
James Michael Hunter Turner <michael@turneratlanta.com>
2025-12-10 02:36:18 -0500

    Update and rename steps.js to steps.ts

--- src/steps.js
+++
@@ -1,8 +0,0 @@
-export const steps = [
-  { id: 0, name: 'Concept' },
-  { id: 1, name: 'Entity' },
-  { id: 2, name: 'Register' },
-  { id: 3, name: 'Marketing' },
-  { id: 4, name: 'Finance' },
-  { id: 5, name: 'Website' }
-];
---
+++ src/steps.ts
@@ -0,0 +1,11 @@
+export const steps = [
+  { id: 0, name: 'Start'},
+  { id: 1, name: 'Concept' },
+  { id: 2, name: 'Structure' },
+  { id: 3, name: 'Name' },
+  { id: 4, name: 'Regisration' },
+  { id: 5, name: 'Financing' },
+  { id: 6, name: 'Accounting' },
+  { id: 7, name: 'Marketing' },
+  { id: 8, name: 'Domain' }
+];
